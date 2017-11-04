2375ec993888f20f17af24e487d3ce252629b51d
Kamil Chlebek <kamil.chlebek94@gmail.com>
2017-11-04 20:07:56 +0100

    lab04 align image, change mock ids

--- lab04/src/app/bike.mock.ts
+++ lab04/src/app/bike.mock.ts
@@ -1,19 +1,19 @@
 import {Bike} from './bike';
 
 const bikes = [
-  new Bike(5, "Rower 1", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium commodi cum cupiditate debitis dolorem.", 2, {
+  new Bike(1, "Rower 1", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium commodi cum cupiditate debitis dolorem.", 2, {
     value: 1000,
     currency: 'USD'
   }, 0, 'https://angular.io/assets/images/logos/angular/logo-nav@2x.png'),
-  new Bike(5, "Rower 2", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium commodi cum cupiditate debitis dolorem.", 5, {
+  new Bike(2, "Rower 2", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium commodi cum cupiditate debitis dolorem.", 5, {
     value: 1000,
     currency: 'USD'
   }, 0, 'https://angular.io/assets/images/logos/angular/logo-nav@2x.png'),
-  new Bike(5, "Rower 2", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium commodi cum cupiditate debitis dolorem.", 5, {
+  new Bike(3, "Rower 2", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium commodi cum cupiditate debitis dolorem.", 5, {
     value: 1000,
     currency: 'USD'
   }, 0, 'https://angular.io/assets/images/logos/angular/logo-nav@2x.png'),
-  new Bike(5, "Rower 2", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium commodi cum cupiditate debitis dolorem.", 5, {
+  new Bike(4, "Rower 2", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium commodi cum cupiditate debitis dolorem.", 5, {
     value: 1000,
     currency: 'USD'
   }, 0, 'https://angular.io/assets/images/logos/angular/logo-nav@2x.png'),
